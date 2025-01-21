c519d9a0dbd013360141e1ea36e65cb77501e0ff
Faraz Khan <faraz.khan@softech-systems.local>
2025-01-21 12:06:12 +0500

    gitrealeaseandtagspractice

---
+++ src/main/java/com/softech/githubpractice/GithubpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.softech.githubpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softech/githubpractice/GithubpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softech.githubpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
