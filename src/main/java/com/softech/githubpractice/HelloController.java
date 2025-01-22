57fd481a162aecdc9f838622e4fe8e0ee68876b0
Faraz Khan <faraz.khan@softech-systems.local>
2025-01-22 11:00:38 +0500

    final

--- src/main/java/com/softech/githubpractice/HelloController.java
+++ src/main/java/com/softech/githubpractice/HelloController.java
@@ -1,13 +1,19 @@
 package com.softech.githubpractice;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    Logger log= LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/")
 public String helloShow(){
+      log.warn("hello word");
     return "hello faraz";
 }
 
